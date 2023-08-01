8f4ff6348d3bacb8f1b5254efe5bde2726105bcb
Alejandro D <ajdiaz5800@gmail.com>
2023-07-31 21:29:57 -0700

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Alex Diaz on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
